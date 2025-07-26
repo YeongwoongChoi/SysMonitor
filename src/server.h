5e25033caf6885af30cdd34798bddfef63364d84
Yeongwoong Choi <278choi@gmail.com>
2025-07-26 14:18:16 +0900

    Edit passed parameters and enlarge BUF_SIZE

--- src/server.h
+++ src/server.h
@@ -3,14 +3,14 @@
 #include <netinet/in.h>
 #include <time.h>
 #include <stdarg.h>
-#define BUF_SIZE (1 << 9)
+#define BUF_SIZE (1 << 13)
 #define PORT_NUM 8080
 
 #define LOG_DEFAULT "\033[0m"
 #define LOG_ERROR "\033[0;31m"
 #define LOG_SUCCESS "\033[0;32m"
-#define LOG_INFO "\033[0;34m"
+#define LOG_INFO "\033[0;94m"
 
-// write message on buf if buf is given, otherwise print on stdout
-void print_log(char *buf, const char *log_type, const char *format, ...);
+// Write message on buf if both buf and remained are given, otherwise print on stdout
+void print_log(char **buf, int *remained, const char *log_type, const char *format, ...);
 const char *get_time();
