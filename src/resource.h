074d4966f6ebf14926ee7a1344d752e5ca40a187
Yeongwoong Choi <278choi@gmail.com>
2025-07-27 11:27:14 +0900

    Add struct and functions for memory usage

--- src/resource.h
+++ src/resource.h
@@ -20,9 +20,19 @@ typedef struct {
     unsigned long long guest_nice;
 } CPUStat;
 
+typedef struct {
+    unsigned long long mem_total;
+    unsigned long long mem_free;
+    unsigned long long mem_available;
+    unsigned long long buffers;
+    unsigned long long cached;
+    unsigned long long shared;
+} MemStat;
+
 long get_core_count();
 CPUStat *read_cpu_stats(const int core_count);
+MemStat read_mem_stat();
 double get_cpu_usage(CPUStat prev, CPUStat curr);
-double get_mem_usage();
+double get_mem_proportion(unsigned long long size, unsigned long long mem_total);
 double get_disk_usage(const char *path);
 #endif
