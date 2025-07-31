669272490347088ce14968b1c0fd654451ff294f
Yeongwoong Choi <278choi@gmail.com>
2025-07-31 14:12:49 +0900

    Extend column length for lengthy fields

--- src/modules/disk_module.c
+++ src/modules/disk_module.c
@@ -28,12 +28,12 @@ char *respond_disk_usage() {
 		return NULL;
 
 	g_logger(&p, &remained, LOG_INFO,
-		"%-82s\
-		\n==================================================================================",
+		"%-85s\
+		\n=====================================================================================",
 		"[SysMonitor] Disk Usages");
 	g_logger(&p, &remained, LOG_INFO,
-		"|     %-10s |    %-13s |           %-10s      | %-15s|\
-		\n==================================================================================",
+		"|     %-10s |    %-16s |      %9s          |  %-15s |\
+		\n=====================================================================================",
 		"Device", "Mounted at", "Size", "Proportion (%)");
 	
 	struct statvfs stat;
@@ -44,14 +44,14 @@ char *respond_disk_usage() {
 			unsigned long long disk_used = disk_total - disk_free;
 			
 			char *tmp = strdup(convert_unit(disk_used));
-			g_logger(&p, &remained, LOG_INFO, "| %-14s | %-17s| %11s / %11s | %12.2lf %% |",
+			g_logger(&p, &remained, LOG_INFO, "| %-14s | %-20s| %10s / %10s | %14.2lf %% |",
 				infos[i].device, infos[i].mountpoint, tmp, convert_unit(disk_total),
 				get_proportion(disk_used, disk_total));
 			free(tmp);
 		}
 	}
 	g_logger(&p, &remained, LOG_INFO,
-		"==================================================================================");
+		"=====================================================================================");
 	return buf;
 }
 
