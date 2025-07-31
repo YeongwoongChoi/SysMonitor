a0ce69608e17166f32d1c011a16c14a91dde4a0e
Yeongwoong Choi <278choi@gmail.com>
2025-07-31 10:30:05 +0900

    Move sendto() to main() and edit function signatures

--- src/server.c
+++ src/server.c
@@ -6,7 +6,7 @@ void print_log(char **buf, int *remained, const char *log_type, const char *form
     va_list args;
     va_start(args, format);
 
-    char message[500];
+    char message[BUF_SIZE];
     vsnprintf(message, sizeof(message), format, args);
 
     if (buf && remained && *buf && *remained > 0) {
@@ -37,53 +37,32 @@ const char *get_time() {
 	return datetime;
 }
 
-void handle_request(int sock, const char *request_type, struct sockaddr_in *client_addr, 
-	socklen_t address_length) {
-		
-		char buf[BUF_SIZE];
-		snprintf(buf, BUF_SIZE, "bin/modules/%s.so", request_type);
+char *handle_request(int sock, const char *request_type) {
+	char buf[BUF_SIZE];
+	snprintf(buf, BUF_SIZE, "bin/modules/%s.so", request_type);
+	char *error_message = NULL;
 
-		int remained;
-		char *error_message = NULL;
+	void *handler = dlopen(buf, RTLD_NOW);
+	if (!handler) {
+		error_message = dlerror();
+		print_log(NULL, NULL, LOG_ERROR, "Cannot open module %s\n %s\n", buf, error_message);
+		return NULL;
+	}
+	dlerror();		// flush previous error
+	Module* (*get_module)() = dlsym(handler, "get_module");
 
-		void *handler = dlopen(buf, RTLD_NOW);
-		if (!handler) {
-			error_message = dlerror();
-			print_log(NULL, NULL, LOG_ERROR, "Cannot open module %s\n %s\n", buf, error_message);
-
-			char *p = buf;
-			remained = BUF_SIZE;
-			print_log(&p, &remained, LOG_ERROR, "Module not found.");
-			sendto(sock, p, BUF_SIZE - remained, 0, (struct sockaddr *)client_addr, address_length);
-			return;
-		}
-		dlerror();		// flush previous error
-		Module* (*get_module)() = dlsym(handler, "get_module");
-
-		if (!get_module && (error_message = dlerror())) {
-			print_log(NULL, NULL, LOG_ERROR, "Cannot find symbol 'get_module': %s", error_message);
-			dlclose(handler);
-			return;
-		}
-
-		Module *mod = get_module();
-		if (mod->initialize_module)
-			mod->initialize_module(print_log);
-
-		char *result = mod->respond_data();
-
-		if (result) {
-			remained = strlen(result);
-			sendto(sock, result, remained, 0, (struct sockaddr *)client_addr, 
-				address_length);
-			
-			char client_ip[INET_ADDRSTRLEN];
-			inet_ntop(AF_INET, &(client_addr->sin_addr), client_ip, INET_ADDRSTRLEN);
-			print_log(NULL, NULL, LOG_INFO, "Sent %u bytes to client(%s) for request '%s'.", 
-				remained, client_ip, request_type);
-			free(result);
-		}
+	if (!get_module && (error_message = dlerror())) {
+		print_log(NULL, NULL, LOG_ERROR, "Cannot find symbol 'get_module': %s", error_message);
 		dlclose(handler);
+		return NULL;
+	}
+
+	Module *mod = get_module();
+	if (mod->initialize_module)
+		mod->initialize_module(print_log);
+
+	char *result = mod->respond_data();
+	return result;
 }
 
 int main() {
@@ -91,7 +70,7 @@ int main() {
 	struct sockaddr_in client_addr, server_addr;
 
 	socklen_t address_length = sizeof(server_addr);
-	char buf[BUF_SIZE];
+	char buf[BUF_SIZE], client_ip[INET_ADDRSTRLEN];
 
 	if ((socket_descriptor = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
 		print_log(NULL, NULL, LOG_ERROR, "Error occurred while socket() executed.");
@@ -111,7 +90,7 @@ int main() {
 	print_log(NULL, NULL, LOG_INFO, "Monitoring agent started at %s on port %d.", get_time(), PORT_NUM);
 
 	int received;
-	char *request_type;
+	char *request_type, *response;
 
 	while (1) {
 		received = recvfrom(socket_descriptor, buf, BUF_SIZE, 0, 
@@ -124,20 +103,28 @@ int main() {
 		buf[received] = '\0';
 		buf[strcspn(buf, "\r\n")] = '\0';
 		request_type = strdup(buf);
-
-		if (!strcmp(request_type, "cpu") || !strcmp(request_type, "mem") || !strcmp(request_type, "disk"))
-			handle_request(socket_descriptor, request_type, &client_addr, address_length);
-        else {
-			char *p = buf;
-			received = BUF_SIZE;
-			memset(buf, 0, BUF_SIZE);
+		
+		char *p = buf;
+		received = BUF_SIZE;
+		memset(buf, 0, BUF_SIZE);
+		
+		if (!strcmp(request_type, "cpu") || !strcmp(request_type, "mem") || !strcmp(request_type, "disk")) {
+			response = handle_request(socket_descriptor, request_type);
+			if (response) {
+				print_log(&p, &received, LOG_INFO, response);
+				inet_ntop(AF_INET, &(client_addr.sin_addr), client_ip, INET_ADDRSTRLEN);
+				print_log(NULL, NULL, LOG_INFO, "Sent %u bytes to client(%s) for request '%s'.", 
+					BUF_SIZE - received, client_ip, request_type);
+			}
+			else
+				print_log(NULL, NULL, LOG_ERROR, "Error occurred while getting response from module.");
+		}
+        else
             print_log(&p, &received, LOG_ERROR, "[SysMonitor] Invalid request. %s", 
                 "Request should be one of these: [cpu|mem|disk]", request_type);
-			sendto(socket_descriptor, buf, BUF_SIZE - received, 0, 
+		sendto(socket_descriptor, buf, BUF_SIZE - received, 0, 
 				(struct sockaddr *)(&client_addr), address_length);
-		}
 		free(request_type);
 	}
-	close(socket_descriptor);
 	return 0;
 }
--- src/server.h
+++ src/server.h
@@ -7,10 +7,9 @@
 #include "common.h"
 #define PORT_NUM 8080
 
-// Respond to the client specified by 'client_addr'
-void handle_request(int sock, const char *request_type, struct sockaddr_in *client_addr, 
-	socklen_t address_length);
-    
+// Get response from module specified by 'request_type'
+char *handle_request(int sock, const char *request_type);
+
 // Write message on buf if both buf and remained are given, otherwise print on stdout
 void print_log(char **buf, int *remained, const char *log_type, const char *format, ...);
 const char *get_time();
